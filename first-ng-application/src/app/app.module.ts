164a3decf6053543d97393f5819cef687f138592
Svetloslav15 <sn_novoselski@abv.bg>
2019-03-23 00:05:42 +0200

    add app

---
+++ first-ng-application/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent,
+    ArticlesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
